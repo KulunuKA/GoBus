4a46f2654b529abb5696938930629e444fa5f34f
Kulunu Kasthuri <kulunukasthuri38@gmail.com>
2025-03-04 00:32:27 +0530

    Side bar implemented

--- gobusFE/src/App.jsx
+++ gobusFE/src/App.jsx
@@ -4,14 +4,25 @@ import { Routes, Route } from "react-router-dom";
 import Register from "./views/Register";
 import Home from "./views/HomePage";
 import Dashboard from "./views/Dashboard";
+import MyRoutes from "./views/Route/index";
+import BusOwnerLayout from "./components/Layouts/BusOwner";
+import Bus from "./views/Bus";
+import Employee from "./views/Employee";
 
 function App() {
   return (
     <Routes>
-      <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register/:role" element={<Register />} />
       <Route path="/home" element={<Home />} />
+
+      {/* Bus Owner routes */}
+      <Route path="/busowner" element={<BusOwnerLayout />}>
+        <Route path="dashboard" element={<Dashboard />} />
+        <Route path="bus" element={<Bus />} />
+        <Route path="route" element={<MyRoutes />} />
+        <Route path="employee" element={<Employee />} />
+      </Route>
     </Routes>
   );
 }
---
+++ gobusFE/src/views/Bus/index.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Bus() {
+  return <div>Bus</div>;
+}
